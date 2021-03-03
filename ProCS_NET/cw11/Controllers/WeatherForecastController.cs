f1667d61e8bc405d7067d448f5a7c51988c4cc95
MichalZielinski2 <50801864+MichalZielinski2@users.noreply.github.com>
2021-03-03 19:50:57 +0100

    Add files via upload

---
+++ ProCS_NET/cw11/Controllers/WeatherForecastController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw11.ComunicationModels;
+using cw11.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace cw11.Controllers
+{
+    [ApiController]
+    [Route("clinic")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+       
+        IDatabaseComunication _db;
+        public WeatherForecastController(IDatabaseComunication db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public IActionResult Post()  //załadowanie do bazy danych przykładowych danych
+        {
+            try
+            {
+                _db.DatabaseExampleData();
+            }catch(Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+            return Ok();
+        }
+
+        [HttpGet("doctors")]
+        public IActionResult GetDoctors()  //wiem literówka. tylko nie wiem czy napisane powinno być lekarza czy lekarzy "która pozwoli nam pobierać dane lekarze"
+        {//zakładam lekarzy bo lekarze to liczba mnoga więc jest blirzej
+            try
+            {
+                var ds = _db.GetDoctors();
+                return Ok(ds);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+            
+        }
+
+        [HttpPost("doctors")]
+        public IActionResult AddDoctor(AddDoctor dr)  
+        {
+            try
+            {
+                _db.AddDoctor(dr);
+                return Ok(dr);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+
+        }
+
+        [HttpPatch("doctors")]
+        public IActionResult ModyfyDoctor(ModyfyDoctor dr)
+        {
+            try
+            {
+                _db.ModDoctor(dr);
+                return Ok(dr);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+
+        }
+
+        [HttpDelete("doctors/{id}")]
+        public IActionResult DeleteDoctor(int id)
+        {
+            try
+            {
+                _db.DeleteDoctor(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+
+        }
+
+    }
+}
---
+++ ProCS_NET/cw11/Models/DoctorsDbContext.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw11.Models
+{
+    //"Data Source=db-mssql;Initial Catalog=s18889;Integrated Security=True"
+    public class DoctorsDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<Prescription> Prescription { get; set; }
+        public DbSet<Prescription_Medicament> Prescription_Medicament { get; set; }
+        public DbSet<Medicament> Medicament { get; set; }
+
+        public DoctorsDbContext()
+        {
+
+        }
+
+        public DoctorsDbContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Doctor>((builder) =>
+            {
+                builder.HasKey(a => a.IdDoctor);
+                //builder.Property(a => a.IdDoctor).ValueGeneratedOnAdd();
+                builder.Property(a => a.FirstName).IsRequired();
+
+                builder.HasMany(a => a.Prescriptions)
+                       .WithOne(a => a.Doctor)
+                       .HasForeignKey(a => a.IdDoctor)
+                       .IsRequired();
+            });
+
+            modelBuilder.Entity<Medicament>((builder) =>
+            {
+                builder.HasKey(a => a.IdMedicament);
+                builder.Property(a => a.IdMedicament).ValueGeneratedOnAdd();
+                builder.Property(a => a.Name).IsRequired();
+
+                builder.HasMany(a => a.Prescription_Medicaments)
+                       .WithOne(a => a.Medicament)
+                       .HasForeignKey(a => a.IdMedicament)
+                       .IsRequired();
+            });
+
+            modelBuilder.Entity<Patient>((builder) =>
+            {
+                builder.HasKey(a => a.IdPatient);
+                builder.Property(a => a.IdPatient).ValueGeneratedOnAdd();
+                builder.Property(a => a.FirstName).IsRequired();
+
+                builder.HasMany(a => a.Prescriptions)
+                       .WithOne(a => a.Patient)
+                       .HasForeignKey(a => a.IdPatient)
+                       .IsRequired();
+            });
+
+            modelBuilder.Entity<Prescription_Medicament>((builder) =>
+            {
+                builder.HasKey(a => new { a.IdPrescription, a.IdMedicament });
+                builder.Property(a => a.IdPrescription).IsRequired();
+                builder.Property(a => a.IdMedicament).IsRequired();
+
+            });
+
+            modelBuilder.Entity<Prescription>((builder) =>
+            {
+                builder.HasKey(a => a.IdPrescription);
+                builder.Property(a => a.IdPrescription).ValueGeneratedOnAdd();
+
+
+
+                builder.HasMany(a => a.Prescription_Medicaments)
+                       .WithOne(a => a.Prescription)
+                       .HasForeignKey(a => a.IdPrescription)
+                       .IsRequired();
+            });
+
+        }
+
+    }
+}
